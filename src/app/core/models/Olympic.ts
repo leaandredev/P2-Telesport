10e708bd870eabef4b2523d2f6d125327fe52d30
Lea ANDRE <aleaandre@gmail.com>
2024-11-06 10:58:25 +0100

    P2-US1-ST1 : create olympic and participation models

--- src/app/core/models/Olympic.ts
+++ src/app/core/models/Olympic.ts
@@ -1,9 +1,9 @@
-// TODO: create here a typescript interface for an olympic country
-/*
-example of an olympic country:
-{
-    id: 1,
-    country: "Italy",
-    participations: []
+import { Participation } from './Participation';
+
+export class Olympic {
+    constructor(
+        public id: number,
+        public country: string,
+        public participations: Participation[]
+    ) {}
 }
-*/
