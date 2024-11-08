598dfafa392c9703f9d16bb3cd5864fa303959de
Lea ANDRE <aleaandre@gmail.com>
2024-11-08 10:21:55 +0100

    P2-US3-ST2 : Put a header and some style on titles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { HeaderComponent } from './core/components/header/header.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgxChartsModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgxChartsModule, HeaderComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
