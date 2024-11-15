135c48b6e19e1ac0682e517a8dee41ae6855fca1
Lea ANDRE <aleaandre@gmail.com>
2024-11-15 17:06:45 +0100

    P2-US5-ST5 : destroy observable when pages components destroyed

--- src/app/pages/country-details/country-details.component.ts
+++ src/app/pages/country-details/country-details.component.ts
@@ -1,6 +1,12 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { map } from 'd3';
+import { Subscription } from 'rxjs';
 import { Olympic } from 'src/app/core/models/Olympic.interface';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 import { NgxLineData } from 'src/app/core/type/ngxDataArray.type';
@@ -11,7 +17,7 @@ import { NgxLineData } from 'src/app/core/type/ngxDataArray.type';
   styleUrl: './country-details.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CountryDetailsComponent implements OnInit {
+export class CountryDetailsComponent implements OnInit, OnDestroy {
   public olympic!: Olympic;
   public dataLineChart: NgxLineData[] = [];
   public numberOfEntries: number = 0;
@@ -19,6 +25,7 @@ export class CountryDetailsComponent implements OnInit {
   public totalNumberOfAthletes: number = 0;
   public errorMessage: string | null = null;
   public loading: boolean = false;
+  private subscription!: Subscription;
 
   constructor(
     private olympicService: OlympicService,
@@ -30,28 +37,33 @@ export class CountryDetailsComponent implements OnInit {
     const countryName = this.route.snapshot.params['id'];
 
     if (countryName) {
-      this.olympicService.getOlympicByName(countryName).subscribe((olympic) => {
-        if (!olympic) {
-          this.errorMessage = `Country ${countryName} not found.`;
-          return;
-        }
-        this.olympic = olympic;
+      this.subscription = this.olympicService
+        .getOlympicByName(countryName)
+        .subscribe((olympic) => {
+          if (!olympic) {
+            this.errorMessage = `Country ${countryName} not found.`;
+            return;
+          }
+          this.olympic = olympic;
 
-        this.dataLineChart =
-          this.olympicService.formatDetailsDataForNgxLineChart(this.olympic);
+          this.dataLineChart =
+            this.olympicService.formatDetailsDataForNgxLineChart(this.olympic);
 
-        this.numberOfEntries = this.olympic.participations.length;
-        this.totalNumberOfMedals = this.olympicService.getTotalMedalsPerCountry(
-          this.olympic
-        );
+          this.numberOfEntries = this.olympic.participations.length;
+          this.totalNumberOfMedals =
+            this.olympicService.getTotalMedalsPerCountry(this.olympic);
 
-        this.totalNumberOfAthletes =
-          this.olympicService.getTotalAthletesPerCountry(this.olympic);
-        this.loading = false;
-      });
+          this.totalNumberOfAthletes =
+            this.olympicService.getTotalAthletesPerCountry(this.olympic);
+          this.loading = false;
+        });
     } else {
       this.errorMessage = 'CountryName URL parameter is missing.';
       this.loading = false;
     }
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { Observable, of, Subscription } from 'rxjs';
 import { Olympic } from 'src/app/core/models/Olympic.interface';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 import { NgxDataArray } from 'src/app/core/type/ngxDataArray.type';
@@ -10,13 +10,14 @@ import { NgxDataArray } from 'src/app/core/type/ngxDataArray.type';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   public olympics$: Observable<Olympic[]> = of([]);
   public dataPieChart: NgxDataArray[] = [];
   public numberOfJOs: number = 0;
   public numberOfCountries: number = 0;
   public errorMessage: string | null = null;
   public loading: boolean = false;
+  private subscription!: Subscription;
 
   constructor(private olympicService: OlympicService, private router: Router) {}
 
@@ -24,7 +25,7 @@ export class HomeComponent implements OnInit {
     this.loading = true;
 
     this.olympics$ = this.olympicService.getOlympics();
-    this.olympics$.subscribe({
+    this.subscription = this.olympics$.subscribe({
       next: (data) => {
         this.dataPieChart =
           this.olympicService.formatOlympicDataForNgxCharts(data);
@@ -37,6 +38,10 @@ export class HomeComponent implements OnInit {
         this.loading = false;
       },
     });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   onCountrySelected(countryName: string): void {
